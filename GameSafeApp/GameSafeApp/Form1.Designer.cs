843a5ff7f1fec41b032fc219f35b5b1b0359ba8a
Радмир Гумеров <radmirgk@mail.ru>
2022-09-30 23:33:12 +0300

    game without images

---
+++ GameSafeApp/GameSafeApp/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace GameSafeApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите размер сейфа";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(457, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Очистить поле";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(30, 109);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(993, 548);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(281, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Создать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GameSafeApp.Image.cropped_Grey_Background_1;
+            this.ClientSize = new System.Drawing.Size(1043, 675);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ GameSafeApp/GameSafeApp/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GameSafeApp
+{
+    public partial class Form1 : Form
+    {
+        private int _sumElems;
+
+        private int _sizeSafe;
+
+        private Button[,] _btnMass;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || !int.TryParse(textBox1.Text, out _sizeSafe) || _sizeSafe < 2)
+            {
+                textBox1.Text = "";
+                return;
+            }
+
+            var rand = new Random();
+            var temp = rand.Next(2);
+
+            _sumElems = _sizeSafe * _sizeSafe * temp;
+
+            _btnMass = new Button[_sizeSafe, _sizeSafe];
+
+            int left = 10;
+
+            for (int i = 0; i < _sizeSafe; i++)
+            {
+                int top = 10;
+
+                for (int j = 0; j < _sizeSafe; j++)
+                {
+
+                    _btnMass[i, j] = new Button();
+                    _btnMass[i, j].Left = left;
+                    _btnMass[i, j].Top = top;
+
+                    _btnMass[i, j].Name = Convert.ToString(i * _sizeSafe + j);
+                    _btnMass[i, j].Click += ButtonOnClick;
+
+                    _btnMass[i, j].Text = "" + temp;
+                    _sumElems += temp;
+
+                    this.panel1.Controls.Add(_btnMass[i, j]);
+                    top += _btnMass[i, j].Height + 2;
+                }
+
+                left += 80;
+            }
+
+            CreateSafe();
+        }
+
+        private void ButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            if (sender is Button button)
+            {
+                var n = Convert.ToInt32(button.Name);
+
+                int _i = n / _sizeSafe, _j = n % _sizeSafe;
+
+                ChangeButtons(_i, _j);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            //var result = 0;
+
+            //for(int i = 0; i < )
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            textBox1.Text = "";
+        }
+
+        private bool SafeIsOpen()
+        {
+            var sum = SumOfSafe();
+
+            return sum == 0 || sum == _sizeSafe * _sizeSafe;
+        }
+
+        private int SumOfSafe()
+        {
+            var sum = 0;
+
+            for (int i = 0; i < _sizeSafe; i++)
+            {
+                for (int j = 0; j < _sizeSafe; j++)
+                {
+                    sum += Convert.ToInt32(_btnMass[i, j].Text);
+                }
+            }
+
+            return sum;
+        }
+
+        private void CreateSafe()
+        {
+            var rnd = new Random();
+
+            var k = 0;
+
+            //var sum = SumOfSafe();
+
+            while (k < 15 || _sumElems == _btnMass.Length || _sumElems == 0)
+            {
+                var _i = rnd.Next(_sizeSafe);
+                var _j = rnd.Next(_sizeSafe);
+
+                ChangeButtons(_i, _j);
+
+                k++;
+            }
+        }
+
+        private void ChangeButtons(int _i, int _j)
+        {
+            for (int i = 0; i < _sizeSafe; i++)
+            {
+                if (_btnMass[i, _j].Text == "0")
+                {
+                    _btnMass[i, _j].Text = "1";
+                    _sumElems += 1;
+                }
+                else if (_btnMass[i, _j].Text == "1")
+                {
+                    _btnMass[i, _j].Text = "0";
+                    _sumElems -= 1;
+                }
+
+                if (_btnMass[_i, i].Text == "0" && _j != i)
+                {
+                    _btnMass[_i, i].Text = "1";
+                    _sumElems += 1;
+                }
+                else if (_btnMass[_i, i].Text == "1" && _j != i)
+                {
+                    _btnMass[_i, i].Text = "0";
+                    _sumElems -= 1;
+                }
+            }
+        }
+    }
+}
